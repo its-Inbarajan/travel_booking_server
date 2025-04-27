efb58741867d7480300bea67c05eea4eba9036c8
inbarajan <inbarajan.wizinoa@gmail.com>
2025-04-27 17:01:32 +0530

    update dist

--- dist/server.js
+++ dist/server.js
@@ -19,7 +19,7 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 // cors
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:8000",
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
 }));
